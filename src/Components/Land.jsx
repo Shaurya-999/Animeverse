b3575d63dca97ca0c4cd3cc27e41e49e258e9889
Shaurya-999 <shauryabisht707@gmail.com>
2025-08-12 07:38:48 +0530

    Changed some info

--- src/Components/Land.jsx
+++ src/Components/Land.jsx
@@ -11,7 +11,7 @@ function Land(){
     <div id="land" className="md:h-[90vw] h-[2000px]">
     <div>
      <h1 className="text-white md:text-[64px]  text-[54px] font-bold text-center mt-[4%] ">Welcome to AnimeVerse ⚡⚡</h1>
-     <p className="text-white font-medium text-center mt-[0.5%] pl-[0.5%] pr-[0.5%]">Welcome to our Anime & Manga Explorer! Dive into detailed information about your favorite anime and manga series, discover top-rated titles, explore characters and staff, and stay updated with seasonal releases — all powered by the Jikan API, providing rich data directly from MyAnimeList without any hassle. Whether you're a casual viewer or a dedicated fan, this page helps you find and learn more about the anime and manga worlds you love.</p>
+     <p className="text-white font-medium text-center mt-[0.5%] pl-[0.5%] pr-[0.5%]">Welcome to Animeverse! Dive into detailed information about your favorite anime and manga series, discover top-rated titles, explore characters, and stay updated with seasonal releases — all powered by the Jikan API, delivering rich data directly from MyAnimeList without any hassle. Whether you're a casual viewer or a dedicated fan, you can easily find the titles you love and save them to your personal watchlist for quick access anytime.</p>
      <div className="flex justify-center mt-[2%]">
     <NavLink to="/home" ><button className="mt-[0.5%] bg-black w-[170px] h-[40px] text-white font-semibold rounded-lg"> Start Watching</button></NavLink>
     </div>
